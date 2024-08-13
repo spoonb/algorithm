2d2588a020c9fc5e8f5e6664c682abaef3a9668d
spoonb <q6666221@gmail.com>
2024-08-13 22:00:51 +0900

    【力扣】#1300 转变数组后最接近目标值的数组和（更新）

--- src/leetcode/editor/cn/P1300_SumOfMutatedArrayClosestToTarget.java
+++ src/leetcode/editor/cn/P1300_SumOfMutatedArrayClosestToTarget.java
@@ -38,6 +38,45 @@ class Solution {
         return sum;
     }
 }
+
+// while (l < r) 中最后的l不会参与计算，想AC只能单独在判定一次l
+//class Solution {
+//    public int findBestValue(int[] arr, int target) {
+//        int l = 0, r = 0;
+//        int res = 0;
+//        int diff = Integer.MAX_VALUE;
+//        for (int i : arr) {
+//            r = Math.max(r, i);
+//            res += i;
+//        }
+//        while (l < r) {
+//            int mid = l + r >> 1;
+//            int sum = sumByValue(arr, mid);
+//            int abs = Math.abs(sum - target);
+//            if (abs < diff || (abs == diff && mid < res)){
+//                res = mid;
+//                diff = abs;
+//            }
+//
+//            if (sum >= target)
+//                r = mid;
+//            else
+//                l = mid + 1;
+//        }
+//        int abs = Math.abs(sumByValue(arr, l) - target);
+//        if (abs < diff || (abs == diff && l < res))
+//            res = l;
+//        return res;
+//    }
+//
+//    private int sumByValue(int[] arr, int value) {
+//        int sum = 0;
+//        for (int i : arr) {
+//            sum += Math.min(i, value);
+//        }
+//        return sum;
+//    }
+//}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
