2e3bfbe3052c531229d2a8be0dfb0c803e00ec5a
spoonb <q6666221@gmail.com>
2024-08-19 00:27:40 +0900

    【力扣】#11 盛最多水的容器（更新思路）

--- src/leetcode/editor/cn/P11_ContainerWithMostWater.java
+++ src/leetcode/editor/cn/P11_ContainerWithMostWater.java
@@ -10,6 +10,11 @@ class Solution {
         while (l < r) {
             int h = Math.min(height[l], height[r]);
             res = Math.max(res, h * (r - l));
+            /*
+                使用移动小的那个
+                原因：l+1或者r-1有可能更大，虽然r-l变小，但h'(min[l,r])*(r-l)会更大。
+                例如：3*7 < 4*6
+             */
             if (height[l] <= height[r])
                 l ++;
             else
