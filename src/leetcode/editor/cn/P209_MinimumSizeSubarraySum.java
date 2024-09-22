76bc74ba1b903464a8074153772dce5c697eaa21
spoonb <q6666221@gmail.com>
2024-09-22 22:38:15 +0900

    【力扣】#209 长度最小的子数组（更新）

--- src/leetcode/editor/cn/P209_MinimumSizeSubarraySum.java
+++ src/leetcode/editor/cn/P209_MinimumSizeSubarraySum.java
@@ -5,19 +5,18 @@ public class P209_MinimumSizeSubarraySum {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int len = nums.length;
         int l = 0, r = 0;
-        int sum = 0;
-        int res = len + 1;
+        int sum = 0, res = Integer.MAX_VALUE;
         while (r < nums.length) {
             sum += nums[r];
             while (sum >= target) {
                 res = Math.min(res, r - l + 1);
-                sum -= nums[l ++];
+                sum -= nums[l];
+                l ++;
             }
             r ++;
         }
-        return res > len ? 0 : res;
+        return res == Integer.MAX_VALUE ? 0 : res;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
